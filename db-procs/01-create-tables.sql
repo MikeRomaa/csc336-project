51d8b746bf9681bdbafbd263a0c1dea8eea7eaaa
KevinZheng0701 <kevinzheng700@gmail.com>
2024-04-11 19:15:43 -0400

    Added home seeker tables

--- db-procs/01-create-tables.sql
+++ db-procs/01-create-tables.sql
@@ -11,6 +11,7 @@ CREATE TABLE user (
     UNIQUE (email)
 );
 
+/*Tutor Time*/
 CREATE TABLE service (
     id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
     name        VARCHAR(100) NOT NULL,
@@ -47,3 +48,49 @@ CREATE TABLE appointment (
     FOREIGN KEY (user_id)    REFERENCES user(id),
     FOREIGN KEY (service_id) REFERENCES service(id)
 );
+
+/*Home Seeker*/
+CREATE TABLE hs_appointment (
+    id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    schedule_id INT UNSIGNED NOT NULL,
+    user_id     INT UNSIGNED NOT NULL,
+    status      VARCHAR(35)  NULL,
+    
+    PRIMARY KEY (id),
+    FOREIGN KEY (schedule_id) REFERENCES schedule(id),
+    FOREIGN KEY (user_id)     REFERENCES user(id)
+);
+
+CREATE TABLE hs_property (
+    id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    broker_id   INT UNSIGNED NOT NULL,
+    address     VARCHAR(64)  NOT NULL,
+    zipcode     VARCHAR(16)  NOT NULL,
+    price       int UNSIGNED NULL,
+    type        VARCHAR(64)  NULL,
+    rooms       INT UNSIGNED NULL,
+    area        FLOAT        NULL,
+    year_built  INT UNSIGNED NULL,
+    
+    PRIMARY KEY (id),
+    FOREIGN KEY (broker_id)    REFERENCES user(id),  
+);
+
+CREATE TABLE hs_schedule (
+    id          INT UNSIGNED NOT NULL, AUTO_INCREMENT,
+    property_id INT UNSIGNED NOT NULL,
+    start       TIME         NOT NULL,
+    end         TIME         NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (property_id) REFERENCES hs_property(id),
+)
+
+CREATE TABLE hs_image (
+    id          INT UNSIGNED NOT NULL, AUTO_INCREMENT,
+    property_id INT UNSIGNED NOT NULL,
+    uri         TEXT         NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (property_id) REFERENCES hs_property(id),
+)

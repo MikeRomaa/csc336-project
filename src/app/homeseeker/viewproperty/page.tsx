f2ebdfd01fd0db90685156a7845731c641db5aee
Mazen Zarrouk <96892759+Mazen-Z@users.noreply.github.com>
2024-05-10 15:19:46 -0400

    updated cards, and where they fit on the page

--- src/app/homeseeker/viewproperty/page.tsx
+++ src/app/homeseeker/viewproperty/page.tsx
@@ -1,96 +1,172 @@
-"use client";
+"use client"
 
-import { Button, Card } from "@tremor/react";
-import { NextPage } from "next";
-import Link from "next/link";
-import { useRouter, useSearchParams } from "next/navigation";
+import { Button, Card, TextInput, NumberInput } from "@tremor/react";
+import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
-
-import { fetchUserDetails } from "@/app/user/actions";
-import { User } from "@/db/auth";
 import { Property } from "@/db/homeseeker/property";
+import Link from "next/link";
 import { Schedule } from "@/db/homeseeker/schedule";
-import { EditPropertyForm } from "./EditPropertyForm";
-import { ScheduleForm } from "./ScheduleForm";
-import { fetchPropertyData, fetchSchedules } from "./actions";
+import { User } from "@/db/auth";
+import Scheduleform from "../../components/scheduleform/page";
+import { fetchPropertyData, fetchSchedules, deletePropertyById, updatePropertyDetails } from "./actions";
+import { fetchUserDetails } from "@/app/user/actions";
 
-const ViewProperty: NextPage = () => {
-	const router = useRouter();
-	const property_id = Number(useSearchParams().get("id"));
-	const [user, setUser] = useState<User | null>(null);
-	const [property, setProperty] = useState<Property | null>(null);
-	const [schedules, setSchedules] = useState<Schedule[] | null>(null);
 
-	useEffect(() => {
-		const fetchDetails = async () => {
-			const propertyData = await fetchPropertyData(property_id);
-			const userData = await fetchUserDetails();
-			const scheduleData = await fetchSchedules(property_id);
-			setProperty(propertyData);
-			setUser(userData);
-			setSchedules(scheduleData);
-		};
-		fetchDetails();
-	}, [property_id]);
+const Viewproperty = () => {
+    const property_id = Number(useSearchParams().get('id'));
+    const [user, setUser] = useState<User | null>(null);
+    const [property, setProperty] = useState<Property | null>(null);
+    const [schedules, setSchedules] = useState<Schedule[] | null>(null);
+    const [editMode, setEditMode] = useState(false);
+    const [editedProperty, setEditedProperty] = useState<Partial<Property>>({});
 
-	const handleFormSubmit = () => {
-		router.refresh();
-	};
+    useEffect(() => {
+        const fetchDetails = async () => {
+            const propertyData = await fetchPropertyData(property_id);
+            const userData = await fetchUserDetails();
+            const scheduleData = await fetchSchedules(property_id);
+            setProperty(propertyData);
+            setUser(userData);
+            setSchedules(scheduleData);
+        }
+        fetchDetails()
+    }, [property_id])
 
-	return (
-		<div className="container">
-			<div className="flex flex-col items-center justify-center w-1/2 pr-4">
-				{property ? (
-					<div className="flex flex-row">
-						<Card className="mt-5 flex flex-row gap-3 justify-center">
-							<p>Properties details:</p>
-							<p>Address: {property.address}</p>
-							<p>Zipcode: {property.zipcode}</p>
-							<p>Type: {property.type}</p>
-							{property.price ? <p>Asking price: {property.price}</p> : null}
-							{property.area ? <p>Area: {property.area}</p> : null}
-							{property.rooms ? <p>Rooms: {property.rooms}</p> : null}
-							{property.built ? <p>Year built: {property.built}</p> : null}
-						</Card>
-						<div className="flex flex-col gap-5">
-							{schedules ? (
-								schedules.map((schedule) => (
-									<Card key={schedule.id}>
-										<div className="mb-3 flex flex-col items-center">
-											<p className="text-slate-600 text-sm">
-												Start time: {new Date(schedule.start).toLocaleString()}
-											</p>
-											<p className="text-slate-600 text-sm">
-												End time: {new Date(schedule.end).toLocaleString()}
-											</p>
-											<Link
-												href={{
-													pathname: "/homeseeker/makeappointment",
-													query: { schedule: schedule.id },
-												}}
-											>
-												<Button className="ml-auto">
-													Make an appointment!
-												</Button>
-											</Link>
-										</div>
-									</Card>
-								))
-							) : (
-								<p>No schedules found yet.</p>
-							)}
-						</div>
-					</div>
-				) : null}
-			</div>
-			{user && user.id === property?.broker_id && (
-				<EditPropertyForm property={property} />
-			)}
-			{user && user.id === property?.broker_id && (
-				<ScheduleForm property_id={property_id} onSubmit={handleFormSubmit} />
-			)}
-		</div>
-	);
+        const handleDelete = async () => {
+            const success = await deletePropertyById(property_id);
+            if (success) {
+                alert('Property deleted successfully');
+            } else {
+                alert('Failed to delete the property');
+            }
+        };
+
+        const handleEdit = () => {
+            setEditMode(true);
+            setEditedProperty({
+                address: property?.address,
+                zipcode: property?.zipcode,
+                type: property?.type,
+                rooms: property?.rooms,
+                area: property?.area,
+                price: property?.price,
+                built: property?.built,
+            });
+        };
+        const handleSave = async () => {
+            const success = await updatePropertyDetails(property_id, editedProperty);
+            if (success) {
+                setEditMode(false);
+                alert('Property updated successfully');
+            } else {
+                alert('Failed to update the property');
+            }
+        };
+    
+        const handleCancel = () => {
+            setEditMode(false);
+            setEditedProperty({});
+        };
+        const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+            const { name, value } = e.target;
+            setEditedProperty(prevState => ({
+                ...prevState,
+                [name]: value,
+            }));
+        };
+    
+
+
+        return (
+            <div className="container px-4">
+                
+                <div className="flex flex-row justify-between items-start w-full space-x-4">
+                    
+                    <div className="w-3/4"> 
+                        
+                        {property ? (
+                            <>
+                                <Card className="mt-5 p-5 bg-white rounded-lg"> 
+                                    <div className="grid grid-cols-2 gap-4 text-lg">
+                                        {property.address ? <p><strong>Address:</strong> {property.address}</p> : null}
+                                        {property.zipcode ? <p><strong>Zipcode:</strong> {property.zipcode}</p> : null}
+                                        {property.type ? <p><strong>Type:</strong> {property.type}</p> : null}
+                                        {property.price ? <p><strong>Asking Price:</strong> ${property.price?.toLocaleString()}</p> : null}
+                                        {property.area ? <p><strong>Area:</strong> {property.area} sq ft</p> : null}
+                                        {property.rooms ? <p><strong>Rooms:</strong> {property.rooms}</p> : null}
+                                        {property.built ? <p><strong>Year Built:</strong> {property.built}</p> : null}
+                                    </div>
+                                </Card>
+                                {user && user.id === property?.broker_id && (
+                                    <div className="mt-4 flex justify-center space-x-4"> {/* Button container for editing */}
+                                        {!editMode ? (
+                                            <>
+                                                <Button onClick={handleEdit}>Edit</Button>
+                                                <Button onClick={handleDelete} className="bg-red-500">Delete</Button>
+                                            </>
+                                        ) : (
+                                            <>
+                                                <Button onClick={handleSave}>Save</Button>
+                                                <Button onClick={handleCancel} className="bg-red-500">Cancel</Button>
+                                            </>
+                                        )}
+                                    </div>
+                                )}
+                            </>
+                        ) : null}
+                        {editMode && user && user.id === property?.broker_id && (
+                            <Card className="mt-5 p-4">
+                                <h2 className="text-lg font-semibold mb-2">Edit Property</h2>
+                                <TextInput name="address" value={editedProperty.address || ''} onChange={handleChange} placeholder="Address" />
+                                <TextInput name="zipcode" value={editedProperty.zipcode || ''} onChange={handleChange} placeholder="Zipcode" />
+                                <TextInput name="type" value={editedProperty.type || ''} onChange={handleChange} placeholder="Type" />
+                                <NumberInput name="rooms" value={editedProperty.rooms || ''} onChange={handleChange} placeholder="Rooms" />
+                                <TextInput name="area" value={editedProperty.area || ''} onChange={handleChange} placeholder="Area" />
+                                <TextInput name="price" value={editedProperty.price || ''} onChange={handleChange} placeholder="Price" />
+                                <TextInput name="built" value={editedProperty.built || ''} onChange={handleChange} placeholder="Year Built" />
+                            </Card>
+                        )}
+                        {user && user.id === property?.broker_id && (
+                <div className="mt-5"> {/* Adjusted width and added mt-5 to match the left column */}
+                    <Scheduleform property_id={property_id} />
+                </div>
+            )}
+                    </div>
+        
+                    
+                <div className="w-1/4 mt-5">
+                    {schedules ? (
+                        schedules.map((schedule) => (
+                            <Card key={schedule.id}>
+                                <div className="mb-3 flex flex-col items-center">
+                                    <p className="text-slate-600 text-sm">
+                                        Start time: {new Date(schedule.start).toLocaleString()}
+                                    </p>
+                                    <p className="text-slate-600 text-sm">
+                                        End time: {new Date(schedule.end).toLocaleString()}
+                                    </p>
+                                    <Link
+                                        href={{
+                                            pathname: "/homeseeker/makeappointment",
+                                            query: { schedule: schedule.id },
+                                        }}
+                                    >
+                                        <Button className="ml-auto">
+                                            Make an appointment!
+                                        </Button>
+                                    </Link>
+                                </div>
+                            </Card>
+                        ))
+                    ) : (
+                        <p>No schedules found yet.</p>
+                    )}
+                </div>
+            
+                    
+                </div>
+            </div>
+        );
 };
 
-export default ViewProperty;
+export default Viewproperty;

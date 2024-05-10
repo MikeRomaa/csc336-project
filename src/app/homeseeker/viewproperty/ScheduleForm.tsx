52d6c6f30f9adadf958db01851416e7cd50a5228
Mazen Zarrouk <96892759+Mazen-Z@users.noreply.github.com>
2024-05-10 15:28:42 -0400

    small update for container

--- src/app/homeseeker/viewproperty/ScheduleForm.tsx
+++ src/app/homeseeker/viewproperty/ScheduleForm.tsx
@@ -1,109 +1,97 @@
 "use client";
 
 import { Button, Card } from "@tremor/react";
-import { DatePicker, DatePickerValue } from "@tremor/react";
-import React, { useState } from "react";
+import { useState } from "react";
+import { makeSchedule } from "@/app/components/scheduleform/actions";
 
-import { makeSchedule } from "./actions";
+const Scheduleform = ({ property_id }: { property_id: number }) => {
+    const [input, setInput] = useState({
+        date: "",
+        start: "",
+        end: "",
+    });
+    const [error, setError] = useState<string | null>(null);
 
-interface ScheduleFormProps {
-	property_id: number;
-	onSubmit: () => void;
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setInput((prev) => ({ ...prev, [name]: value }));
+    };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (property_id) {
+            const startDateTime = `${input.date}T${input.start}`;
+            const endDateTime = `${input.date}T${input.end}`;
+            const output = await makeSchedule(
+                property_id,
+                new Date(startDateTime),
+                new Date(endDateTime)
+            )
+            if (typeof output === "string") {
+                setError(output);
+            } else {
+                setError(null);
+                window.location.reload();
+            }
+        }
+    }
+
+    return (
+        <div className="container mx-auto py-5">
+            {property_id ? (
+                <div>
+                    <Card className="max-w-96 text-center">
+                        <h2>Upload your availability</h2>
+                        <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center gap-2 mt-3">
+                            <div className="flex-1 w-full">
+                                <div className="w-full flex justify-center items-center m-2">
+                                    <label htmlFor="start">Date</label>
+                                    <input
+                                        className="ml-auto"
+                                        required
+                                        name="date"
+                                        type="date"
+                                        value={input.date}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className="w-full flex justify-center items-center m-2">
+                                    <label htmlFor="start">Start</label>
+                                    <input
+                                        className="ml-auto"
+                                        required
+                                        name="start"
+                                        type="time"
+                                        value={input.start}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className="w-full flex justify-center items-center m-2">
+                                    <label htmlFor="end">End</label>
+                                    <input
+                                        className="ml-auto"
+                                        required
+                                        name="end"
+                                        type="time"
+                                        value={input.end}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                            </div>
+                            <div className="h-4">
+                                {error && (
+                                    <small className="text-sm text-red-500">{error}</small>
+                                )}
+                            </div>
+                            <Button type="submit">Add Schedule</Button>
+                        </form>
+                    </Card>
+                </div >
+            ) : (null)
+            }
+        </div >
+    );
 }
 
-export const ScheduleForm: React.FC<ScheduleFormProps> = ({
-	property_id,
-	onSubmit,
-}) => {
-	const [input, setInput] = useState({
-		start: "",
-		end: "",
-	});
-	const [date, setDate] = useState<DatePickerValue | undefined>(undefined);
-	const [error, setError] = useState<string | null>(null);
+export default Scheduleform;
 
-	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const { name, value } = e.target;
-		setInput((prev) => ({ ...prev, [name]: value }));
-	};
-
-	const handleDateChange = (value: DatePickerValue | undefined) => {
-		setDate(value);
-	};
-
-	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		if (date === undefined) {
-			setError("Date not selected");
-			return;
-		}
-		const startDateTime = `${date?.toISOString().split("T")[0]}T${input.start}`;
-		const endDateTime = `${date?.toISOString().split("T")[0]}T${input.end}`;
-		const output = await makeSchedule(
-			property_id,
-			new Date(startDateTime),
-			new Date(endDateTime),
-		);
-		if (typeof output === "string") {
-			setError(output);
-		} else {
-			setError(null);
-			onSubmit();
-		}
-	};
-
-	return (
-		<div className="container mx-auto py-20">
-			{property_id ? (
-				<div>
-					<Card className="max-w-96 text-center">
-						<h2>Upload your availability</h2>
-						<form
-							onSubmit={handleSubmit}
-							className="flex flex-col justify-center items-center gap-2 mt-3"
-						>
-							<div className="flex-1 w-full">
-								<div className="w-full flex justify-center items-center m-2">
-									<DatePicker
-										className="mx-auto max-w-sm"
-										placeholder="Select a date"
-										value={date}
-										onValueChange={handleDateChange}
-									/>
-								</div>
-								<div className="w-full flex justify-center items-center m-2">
-									<label htmlFor="start">Start</label>
-									<input
-										className="ml-auto"
-										required
-										name="start"
-										type="time"
-										value={input.start}
-										onChange={handleChange}
-									/>
-								</div>
-								<div className="w-full flex justify-center items-center m-2">
-									<label htmlFor="end">End</label>
-									<input
-										className="ml-auto"
-										required
-										name="end"
-										type="time"
-										value={input.end}
-										onChange={handleChange}
-									/>
-								</div>
-							</div>
-							<div className="h-4">
-								{error && (
-									<small className="text-sm text-red-500">{error}</small>
-								)}
-							</div>
-							<Button type="submit">Add Schedule</Button>
-						</form>
-					</Card>
-				</div>
-			) : null}
-		</div>
-	);
-};

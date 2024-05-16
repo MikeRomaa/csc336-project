ae981147bd13e80987f57f526238172f5b8383d8
Mazen Zarrouk <96892759+Mazen-Z@users.noreply.github.com>
2024-05-15 21:37:43 -0400

    Update Styling for Navbar.tsx (with npm lint fixes)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,69 +3,61 @@
 import Link from "next/link";
 import type React from "react";
 
-import type { User } from "@/db/auth";
+import { fetchUserDetails } from "./actions"
 import { useEffect, useState } from "react";
-import { fetchUserDetails } from "./actions";
+import type { User } from "@/db/auth";
+
+
+
 
 export const Navbar: React.FC = () => {
-	const [currUser, setUser] = useState<null | User>(null);
+	const [currUser, setUser] = useState<null | User>(null)
 	useEffect(() => {
 		async function user() {
-			const u = await fetchUserDetails();
-			setUser(u);
-			console.log(u);
+			const u = await fetchUserDetails()
+			setUser(u)
+			console.log(u)
 		}
-		user();
-	}, []);
+		user()
+
+	}, [])
 	return (
 		<nav className="bg-white border-b-4 border-black">
 			<ul className="flex flex-row justify-between items-center p-4">
 				<li>
 					<Link href="/">
-						<span className="font-semibold text-2xl text-center text-blue-900">
-							Home
-						</span>
+						<span className="font-semibold text-2xl text-center text-blue-900">Home</span>
 					</Link>
 				</li>
 				<li>
 					<Link href="/homeseeker">
-						<span className="font-semibold text-2xl text-center text-blue-900">
-							Homeseeker
-						</span>
+						<span className="font-semibold text-2xl text-center text-blue-900">Homeseeker</span>
 					</Link>
 				</li>
 				<li>
 					<Link href="/tutortime">
-						<span className="font-semibold text-2xl text-center text-blue-900">
-							TutorTime
-						</span>
+						<span className="font-semibold text-2xl text-center text-blue-900">TutorTime</span>
 					</Link>
 				</li>
 				<li>
 					<Link href="/user">
-						<span className="font-semibold text-2xl text-center text-blue-900">
-							Account
-						</span>
+						<span className="font-semibold text-2xl text-center text-blue-900">Account</span>
 					</Link>
 				</li>
-				{!currUser ? (
+				{!currUser ?
 					<li>
 						<Link href="/auth/sign-in">
-							<span className="font-semibold text-2xl text-center text-blue-900">
-								Login
-							</span>
+							<span className="font-semibold text-2xl text-center text-blue-900">Login</span>
 						</Link>
 					</li>
-				) : (
+					:
 					<li>
 						<Link href="/">
-							<span className="font-semibold text-2xl text-center text-red-900">
-								Logout
-							</span>
+							<span className="font-semibold text-2xl text-center text-red-900">Logout</span>
 						</Link>
 					</li>
-				)}
+				}
 			</ul>
 		</nav>
-	);
+	)
 };
